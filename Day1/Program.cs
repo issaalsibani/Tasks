ea0924d6dbf2c7e8a1c3d1c9c449716fcd567960
Issa Al Sibani <issaalsibani12@gmail.com>
2026-07-01 12:26:13 +0400

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name :");
+            Console.WriteLine("Enter your age: ");
+            Console.WriteLine("Enter your height: ");
+
+            String userName = Console.ReadLine();
+            int userAge = int.Parse(Console.ReadLine());
+            float userHeight = float.Parse(Console.ReadLine());
+            bool student = userAge >= 6 && userAge <= 23;
+
+            Console.Write("Name:" + userName);
+            Console.Write(" Age:" + userAge);
+            Console.Write(" Height:" + userHeight);
+            Console.Write(" Student:" + student);
+
+        }
+    }
+}
