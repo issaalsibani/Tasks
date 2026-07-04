0919bc281c7ac21a78e42b80f654f9e4e7e61223
Issa Al Sibani <issaalsibani12@gmail.com>
2026-07-04 09:25:21 +0400

    Task1 from practice 2

---
+++ Day2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1 - Countdown Timer
+
+            Console.WriteLine("Enter a starting number: ");
+            int startingNumber = int.Parse(Console.ReadLine()); 
+            int counter = 0;
+            for (counter = startingNumber; counter >= 1; counter --) 
+
+            {
+                Console.WriteLine(counter);
+            }
+            Console.WriteLine("Liftoff!");
+        }
+    }
+}
