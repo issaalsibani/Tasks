525162a78b69fdac24d1f4a61eb0e0881da6dcb4
Issa Al Sibani <issaalsibani12@gmail.com>
2026-07-06 21:19:06 +0400

    Task  3 (practice 4)

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -7,10 +7,17 @@
         {
             Console.WriteLine("Welcome " + name);
         }
+
         public static int Square(int number)
         {
             int result = number * number;
             return result;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            double fahrenheit = (celsius * 9.0 / 5.0) + 32;
+            return fahrenheit;
         }
         static void Main(string[] args)
         {
@@ -22,7 +29,7 @@
             */
             //////////////////////////////////////////////////////////////////////
             //Task 2 - Square Number Function
-            
+            /*
             Console.WriteLine("Enter a number: ");
             int userNumber = int.Parse(Console.ReadLine());
             
@@ -32,8 +39,14 @@
             Console.WriteLine("The square is: " + squaredNumber);
             */
             ////////////////////////////////////////////////////////
-            
+            //Task 3 - Celsius to Fahrenheit Function
 
+            Console.Write("Enter the temperature in Celsius: ");
+            double userCelsius = double.Parse(Console.ReadLine());
+
+            double result = CelsiusToFahrenheit(userCelsius);
+
+            Console.WriteLine("Temperature in Fahrenheit: " + result);
         }
     }
 }
