2cf4e9b265595c519029424f3bc30e5d7c545869
Issa Al Sibani <issaalsibani12@gmail.com>
2026-07-06 21:15:10 +0400

    Task 2 (practice 4)

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -7,13 +7,31 @@
         {
             Console.WriteLine("Welcome " + name);
         }
+        public static int Square(int number)
+        {
+            int result = number * number;
+            return result;
+        }
         static void Main(string[] args)
         {
             //Task 1 - Personalized Welcome Function
+            /*
             Console.WriteLine("Enter your name: ");
             String user = Console.ReadLine();
             Print (user);
+            */
             //////////////////////////////////////////////////////////////////////
+            //Task 2 - Square Number Function
+            
+            Console.WriteLine("Enter a number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            
+
+            int squaredNumber = Square(userNumber);
+
+            Console.WriteLine("The square is: " + squaredNumber);
+            */
+            ////////////////////////////////////////////////////////
             
 
         }
