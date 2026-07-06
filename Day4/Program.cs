77f3ff29589be2150547eaface01a18423f68a3f
Issa Al Sibani <issaalsibani12@gmail.com>
2026-07-06 21:00:19 +0400

    Task1 (Practice 4)

---
+++ Day4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Day4
+{
+    internal class Program
+    {
+        
+        public static void Print(string name)
+        {
+            Console.WriteLine("Welcome " + name);
+        }
+        static void Main(string[] args)
+        {
+            //Task 1 - Personalized Welcome Function
+            Console.WriteLine("Enter your name: ");
+            String user = Console.ReadLine();
+            Print (user);
+            //////////////////////////////////////////////////////////////////////
+            
+
+        }
+    }
+}
